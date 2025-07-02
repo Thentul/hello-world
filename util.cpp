11acdd1caa3436085d50448940d175ab426bcf3e
Thent <no@address.com>
2025-07-02 16:25:05 -0400

    testing

--- util.cpp
+++ util.cpp
@@ -1,2 +1,15 @@
 #include <random>
 
+class rng
+{
+    private:
+
+    public:
+    int generate()
+    {
+        std::random_device rd;
+        std::mt19937 eng(rd());
+        std::uniform_int_distribution<int> dist(1, 6);
+        return dist(eng);
+    }
+};
